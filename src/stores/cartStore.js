162848130dd075fbc0cc85bcfde377c7ec7127b5
Administrator <1250702640@qq.com>
2023-07-26 09:50:23 +0800

    购物车单选功能

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -23,7 +23,11 @@ export const useCartStore=defineStore('cart',()=>{
         const idx=cartList.value.findIndex((item)=>skuId===item.skuId)
         cartList.value.splice(idx,1)
     }
-
+//单选功能singlecheck
+const singleCheck=(skuId,selected)=>{
+    const item=cartList.value.find((item)=>item.skuId===skuId)
+    item.selected=selected
+}
 //计算属性（总数量、总价格)
 const allCount=computed(()=>cartList.value.reduce((a,c)=>a+c.count,0))
 const allPrice=computed(()=>cartList.value.reduce((a,c)=>a+c.count*c.price,0))
@@ -32,7 +36,8 @@ const allPrice=computed(()=>cartList.value.reduce((a,c)=>a+c.count*c.price,0))
         addCart,
         delCart,
         allCount,
-        allPrice
+        allPrice,
+        singleCheck
     }
 },{
     persist:true,
--- src/views/CartList/index.vue
+++ src/views/CartList/index.vue
@@ -1,6 +1,11 @@
 <script setup>
 import { useCartStore } from '@/stores/cartStore'
 const cartStore = useCartStore()
+//单选回调
+const singleCheck=(i,selected)=>{
+    console.log(i,selected);
+    cartStore.singleCheck(i.skuId,selected)
+}
 </script>
 
 <template>
@@ -25,7 +30,7 @@ const cartStore = useCartStore()
             <tr v-for="i in cartStore.cartList" :key="i.id">
               <td>
                 <!-- 单选框 -->
-                <el-checkbox/>
+                <el-checkbox :model-value="i.selected" @change="(selected)=>singleCheck(i,selected)"/>
               </td>
               <td>
                 <div class="goods">
